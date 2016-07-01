f5368a3ae910e21d0c5aa5254f10c16485a7f15a
Liam Daniel Hurt <liamhurt51@gmail.com>
2016-07-01 10:26:42 -0700

    adding sudo code for gamemechanics

---
+++ javascript/game-mechanics.js
@@ -0,0 +1,36 @@
+//56 spaces for the game board
+//-11 each layer
+//faze 1: tiles-45;faze 2: tiles-34;faze 3: tiles-23;
+//each tile picks a random place on the board in sequence
+//area must be populated to place a tile unless its the bottom layer
+
+//A start function
+//-- start is linked to a button
+//-- user is playing function
+
+//populate the game board
+//-- randomly generate if there will be an empty area
+//-- only can populate over a poulated area if z-index is 1 or greater
+//-- must have three different layers
+
+//randomly generate a picture
+//-- must always have pairs
+
+//link the timer to the game
+//-- count down function
+//-- 2:30 mins to pass
+//-- link best timer
+
+//have a score function
+//-- score counts up the more you get right
+//-- highscore function
+
+//gameover function
+//-- displays a game over sign
+//-- resets start button
+
+//win game function
+//-- display score
+//-- display time
+//-- check if it is greater than high score
+//-- check is time is better
